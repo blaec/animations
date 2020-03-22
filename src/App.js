23d1c2f6e0d9219a98bf5c7820ccbbd41ddba326
blaec <odeskonst@yandex.ru>
2020-03-22 22:39:57 +0200

    430.2 Using ReactTransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -33,16 +34,23 @@ class App extends Component {
                     Toggle
                 </button>
                 <br/>
-                {this.state.showBlock ? (
-                    <div
-                        style={{
-                            backgroundColor: 'red',
-                            width: 100,
-                            height: 100,
-                            margin: 'auto'
-                        }}
-                    />
-                ) : null}
+                <Transition in={this.state.showBlock}
+                            timeout={1000}
+                            mountOnEnter
+                            unmountOnExit>
+                    {state => (
+                        <div
+                            style={{
+                                backgroundColor: 'red',
+                                width: 100,
+                                height: 100,
+                                margin: 'auto',
+                                transition: 'opacity 1s ease-out',
+                                opacity: state === 'exiting' ? 0 : 1
+                            }}
+                        />
+                    )}
+                </Transition>
                 {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
                 {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen}/> : null}
                 <button className="Button" onClick={this.showModal}>Open Modal</button>
